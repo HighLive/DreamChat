612228d318cb91427dc2e0810f8d2228baa997d0
kang <rilac1@naver.com>
2021-10-10 17:59:19 +0900

    Prototype implemeted in Flask

--- Flask/app.py
+++ Flask/app.py
@@ -11,30 +11,16 @@ def home():
    return render_template('index.html')
 
 ## POST 요청 API 구현
-@app.route('/review', methods = ['POST'])
+@app.route('/video_num', methods = ['POST'])
 def write_review():
-    title_receive = request.form['title_give']
-    author_receive = request.form['author_give']
-    review_receive = request.form['review_give']
-
-    ## DB에 삽입할 review 딕셔너리 만들기
-    doc = {
-        'title': title_receive,
-        'author': author_receive,
-        'review': review_receive
-    }
     
-    db.bookreview.insert_one(doc)
-    return jsonify({'msg':'리뷰가 성공적으로 작성되었습니다.'})
+    return 
 
 ## GET 요청 API 구현
-@app.route('/review', methods=['GET'])
+@app.route('/video_num', methods=['GET'])
 def read_reviews():
-    # 서버는 클라이언트로부터 데이터를 받을 필요x.
-    # 모든 리뷰를 DB로부터 가져와 내려주기만 하면됨.
-    reviews = list(db.bookreview.find({}, {'_id': False}))
 
-    return jsonify({'all_reviews': reviews})
+    return 
     
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
